b3669884b62b7073e78d4e9eeb00d38fef067b8c
LIK WAI NG <nglikwai@ymail.com>
2022-03-09 00:42:49 +0800

    transparent

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,3 +5,15 @@ for (let button of buttons) {
         button.classList.add('grey')
     })
 }
+
+var currentTime = new Date().getHours();
+
+if (currentTime > 23 || currentTime < 7) {
+    document.querySelector('body').style.backgroundColor = 'rgba(255, 252, 248, 0.7)';
+    const cards = document.querySelectorAll('.card');
+    for (let card of cards) {
+        card.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
+    }
+
+
+}
