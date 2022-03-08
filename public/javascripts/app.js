5df53d8d469919291cd7bdf18db20450fd41186c
LIK WAI NG <nglikwai@ymail.com>
2022-03-09 02:21:39 +0800

    nav

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,28 +5,3 @@ for (let button of buttons) {
         button.classList.add('grey')
     })
 }
-
-var currentTime = new Date().getHours();
-
-if (currentTime > 23 || currentTime < 7) {
-    // document.querySelector('body').style.backgroundColor = 'rgba(255, 252, 248, 0.7)';
-    document.querySelector('body').style.backgroundImage = 'url("http://3.bp.blogspot.com/-8CHSBu1Z0X8/XBKneFT-L_I/AAAAAAAAOmI/33B6cqn86EcpGk_E7h9dBl3m7UlU40eAQCK4BGAYYCw/s1600/night.jpg.png")';
-
-    document.querySelector('body').style.backgroundColor = '#070f16'
-    document.querySelector('nav').style.backgroundColor = 'transparent';
-    const cards = document.querySelectorAll('.card');
-    for (let card of cards) {
-        card.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    }
-    const texts = document.querySelectorAll('.night-text');
-    for (let text of texts) {
-        text.style.color = '#888';
-    }
-    document.querySelector('.user-info').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-
-
-}
-script
-if (currentTime > 23 || currentTime < 7) {
-    document.querySelector('#header-wrap').style.display = 'none'
-}
