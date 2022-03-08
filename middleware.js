6ad7cad3bd0c2e9f7f2f48cce304aa0437f46e29
LIK WAI NG <nglikwai@ymail.com>
2022-03-09 00:07:30 +0800

    idk

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+const User = require('./models/user');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
