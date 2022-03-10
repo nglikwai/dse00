c610f5c31183e0a074315db62cd9937c2582a3a0
LIK WAI NG <nglikwai@ymail.com>
2022-03-11 01:49:00 +0800

    s

--- schemas.js
+++ schemas.js
@@ -29,7 +29,7 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        category: Joi.string().required()
+        category: Joi.string()
     }).required(),
     deleteImages: Joi.array()
 });
