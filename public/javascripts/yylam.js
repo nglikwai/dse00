45f40c9f23106d32eaba7f28772d3744ce974e8b
nglikwai <nglikwai@ymail.com>
2022-03-15 17:55:38 +0800

    api

--- public/javascripts/yylam.js
+++ public/javascripts/yylam.js
@@ -1,6 +1,6 @@
 async function callyylam() {
         try {
-          const response = await axios.get('https:/www.dse00.com/apis/yylam');
+          const response = await axios.get('https://www.dse00.com/apis/yylam');
           console.log(response);
         } catch (error) {
           console.error(error);
