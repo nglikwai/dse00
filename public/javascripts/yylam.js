ba5040306fa23f960363c697bb556a5805cb2762
nglikwai <nglikwai@ymail.com>
2022-03-16 13:10:31 +0800

    a

--- public/javascripts/yylam.js
+++ public/javascripts/yylam.js
@@ -5,7 +5,7 @@ async function callyylam() {
         try {
           const res = await axios.get('https://www.dse00.com/apis/yylam');
           const i = Math.floor(Math.random()*80)
-          yylamdiv.innerText = `${res.data[i].title} - ${res.data[i].author} `;
+          yylamdiv.innerText = `${res.data[i].title}`;
         } 
         catch (error) {
           console.error(error);
