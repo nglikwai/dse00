600a1a27fc112701d44506f9a0615ab5ffe680b1
nglikwai <nglikwai@ymail.com>
2022-03-27 14:20:32 +0800

    a

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,63 +2,66 @@ const Campground = require("../models/campground");
 const Review = require("../models/review");
 const User = require("../models/user");
 
-module.exports.createReview = async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    if (req.user) {
-        const user = await User.findById(req.user._id);
-        review.author = req.user._id;
-        user.reviews.push(review);
-        user.coin += 3;
-        await user.save();
-    }
-    review.post = campground;
-    campground.reviews.push(review);
-    campground.popular +=1 ;
-    await review.save();
-    await campground.save();
+module.exports.createReview = async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  const id = '622874ccc8ed254d82edf591';
+  if (req.user) {
+      const id = req.user._id;
+  }
+  const user = await User.findById(id);
+  review.author = id;
+  user.reviews.push(review);
+  user.coin += 3;
+  await user.save();
 
-    req.flash("success", "成功留言!");
-    res.redirect(`/${campground._id}`);
+  review.post = campground;
+  campground.reviews.push(review);
+  campground.popular += 1;
+  await review.save();
+  await campground.save();
+
+  req.flash("success", "成功留言!");
+  res.redirect(`/${campground._id}`);
 };
 
-module.exports.createIframeReview = async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    if (req.user) {
-        const user = await User.findById(req.user._id);
-        review.author = req.user._id;
-        user.reviews.push(review);
-        user.coin += 3;
-        await user.save();
-    }
-    review.post = campground;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-
-    req.flash("success", "🪙 + 3 ");
-    res.redirect(`/iframe/${campground._id}`);
-};
-
-module.exports.deleteReview = async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
+module.exports.createIframeReview = async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  if (req.user) {
     const user = await User.findById(req.user._id);
-    user.coin -= 3;
+    review.author = req.user._id;
+    user.reviews.push(review);
+    user.coin += 3;
     await user.save();
-    req.flash("success", "成功刪除");
-    res.redirect(`/${id}`);
+  }
+  review.post = campground;
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+
+  req.flash("success", "🪙 + 3 ");
+  res.redirect(`/iframe/${campground._id}`);
 };
 
-module.exports.deleteIframeReview = async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    const user = await User.findById(req.user._id);
-    user.coin -= 3;
-    await user.save();
-    req.flash("success", "成功刪除");
-    res.redirect(`/iframe/${id}`);
+module.exports.deleteReview = async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  const user = await User.findById(req.user._id);
+  user.coin -= 3;
+  await user.save();
+  req.flash("success", "成功刪除");
+  res.redirect(`/${id}`);
+};
+
+module.exports.deleteIframeReview = async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  const user = await User.findById(req.user._id);
+  user.coin -= 3;
+  await user.save();
+  req.flash("success", "成功刪除");
+  res.redirect(`/iframe/${id}`);
 };
