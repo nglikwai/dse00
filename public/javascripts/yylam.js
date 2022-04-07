ce77eac6604db69e8f5d4dcbecbe108854beb569
LIK WAI NG <nglikwai@ymail.com>
2022-04-08 03:14:56 +0800

    update

--- public/javascripts/yylam.js
+++ public/javascripts/yylam.js
@@ -5,7 +5,7 @@ async function callyylam() {
     try {
         const res = await axios.get('https://www.dse00.com/apis/yylam');
         const i = Math.floor(Math.random() * 95)
-        yylamdiv.innerText = `${res.data[i].title}`;
+        yylamdiv.innerText = `💬 ${res.data[i].title}`;
     } catch (error) {
         console.error(error);
     }
