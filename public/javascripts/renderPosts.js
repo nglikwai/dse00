8be7eda4866bcd623ad102484b286a349a82fc2e
LIK WAI NG <nglikwai@ymail.com>
2022-04-13 15:56:14 +0800

    a

--- public/javascripts/renderPosts.js
+++ public/javascripts/renderPosts.js
@@ -11,7 +11,7 @@ const adsense = `
 <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-gw-3+1f-3d+2z"
     data-ad-client="ca-pub-6622218753379872" data-ad-slot="8821319250"></ins>
 <script>
-    ${(adsbygoogle = window.adsbygoogle || []).push({})}
+    ${(adsbygoogle = window.adsbygoogle || []).push({})};
 </script>
 </div>`
 
