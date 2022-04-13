9250de3aa2a2998f99af930aef474b03f9e15e2a
LIK WAI NG <nglikwai@ymail.com>
2022-04-13 15:37:18 +0800

    a

--- public/javascripts/renderPosts.js
+++ public/javascripts/renderPosts.js
@@ -1,3 +1,4 @@
+import 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6622218753379872';
 const firstPostsBody = document.querySelector('#first-posts-body');
 const postBody = document.querySelector('#posts-body');
 
--- public/javascripts/yylam.js
+++ public/javascripts/yylam.js
@@ -15,21 +15,23 @@ async function callyylam() {
 async function createFriendActivity() {
     try {
         const res = await axios.get('https://www.dse00.com/apis/friendlist');
-        const makePosts = (res) => {
-            friendActivity.innerHTML = '';
-            for (let review of res.data.reviews) {
-                const act = document.createElement('div');
-                act.setAttribute('class', 'friend-activity')
-                act.innerHTML = `<a href="/${review.post} "><span style="font-size: 13px; color: #aaa;">
-                <i class="fa-regular fa-comment-dots"></i>  ${review.body.substring(0, 32)}
-            </span></a> -
-        <a href="/users/user/${review.author._id}"> <span style=" font-size: 11px; color: #ccc;">
-                ${review.author.username.toUpperCase()}
-            </span>
-        </a>`
-                friendActivity.appendChild(act)
-            }
+
+        friendActivity.innerHTML = '';
+        for (let review of res.data.reviews) {
+            const act = document.createElement('div');
+            act.setAttribute('class', 'friend-activity')
+            act.innerHTML = `
+            <a href="/${review.post} ">
+                <span style="font-size: 13px; color: #aaa;">
+                    <i class="fa-regular fa-comment-dots"></i>  ${review.body.substring(0, 32)}
+                </span>
+            </a> -
+            <a href="/users/user/${review.author._id}"> 
+                <span style=" font-size: 11px; color: #ccc;">${review.author.username.toUpperCase()}</span>
+            </a>`
+            friendActivity.appendChild(act)
         }
+
     } catch (error) {
         console.error(error);
     }
