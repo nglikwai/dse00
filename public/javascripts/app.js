71c894f638f4e8df9b024c710bba01a6fbc901a7
LIK WAI NG <nglikwai@ymail.com>
2022-05-23 01:20:08 +0800

    styling

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,11 @@
 const buttons = document.querySelectorAll('.disappear-button');
+const typing = document.querySelector('#typing');
 
-
+document.addEventListener('keydown', () => typing.innerHTML = 'typing...')
+document.addEventListener('keyup', () =>
+    setTimeout(() => {
+        typing.innerHTML = 'Online'
+    }, 1000))
 for (let button of buttons) {
     button.addEventListener('click', function () {
         button.classList.add('grey')
--- public/javascripts/yylam.js
+++ public/javascripts/yylam.js
@@ -16,11 +16,14 @@ async function callyylam() {
 async function createFriendList() {
     try {
         const res = await axios.get('https://www.dse00.com/apis/getfriendlist');
+        let i = 0;
         for (let friend of res.data.friendList) {
             let div = document.createElement('div')
             div.setAttribute('class', 'friend-name')
-            div.innerHTML = `<span style="font-size:5px; margin-right:5px">🟢</span><a href="/users/user/${friend._id}"> ${friend.username.toUpperCase()}</a>`;
+            div.innerHTML = `<i class="fa-solid fa-circle" style="font-size:8px; margin-right:5px; color:rgb(180, 227, 187)"></i><a href="/users/user/${friend._id}"> ${friend.username.toUpperCase()}</a>`;
             friendList.append(div)
+            i++
+            if (i == 5) return
         }
     } catch (error) {
         console.error(error);
@@ -33,19 +36,22 @@ async function createFriendActivity() {
         // https://www.dse00.com/apis/friendactivities
 
         friendActivity.innerHTML = '';
+
         for (let review of res.data) {
             const act = document.createElement('div');
             act.setAttribute('class', 'friend-activity')
             act.innerHTML = `
-            <a href="/${review.post} ">
-                <span style="font-size: 13px; color: #aaa;">
-                    <img src="/files/profileLogo.png" style="width:22px; height:24px ; margin:0;"/>  ${review.body.substring(0, 32)}
-                </span>
-            </a> -
-            <a href="/users/user/${review.author._id}"> 
-                <span style=" font-size: 11px; color: #ccc;">${review.author.username.toUpperCase()}</span>
-            </a>`
+                <div style='display:flex; align-items:center;'>
+                <a href="/users/user/${review.author._id}"> <img src="/files/profileLogo.png" style="width:22px; height:24px ; margin: 0 8px 0  0;"/>   </a>
+            
+                <a href="/${review.post} " style="display:flex"><span style="font-size: 13px; color: #999;">${review.body.substring(0, 32)}
+                    <span style=" font-size: 11px; color: #ccc;">- ${review.author.username.toUpperCase()}</span>            
+                </span> </a> 
+                </div>
+           
+         `
             friendActivity.appendChild(act)
+
         }
 
     } catch (error) {
